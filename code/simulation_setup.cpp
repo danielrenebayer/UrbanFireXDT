6b69f51c4eec1f0ebfa4fb049e9392bd4c83626f
Daniel Rene Bayer <daniel.bayer@uni-wuerzburg.de>
2022-03-08 18:16:57 +0100

    Simulation setup now accompanies the new functions introduced in the last three commits (the word functions means tasks in this context)

--- code/simulation_setup.cpp
+++ code/simulation_setup.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 
 #include "global.h"
+#include "units.h"
 
 
 
@@ -43,24 +44,14 @@ bool configld::parse_scenario_file(int scenario_id) {
 			// convert individual strings to int / float / char
 			int currentLineID = stoi( currLineSplitted[0] );
 			if (currentLineID == scenario_id) {
-				global::ts_start_str    = new string( currLineSplitted[1] ); // copy constructor
-				global::ts_end_str      = new string( currLineSplitted[2] ); // copy constructor
-				global::tsteps_per_hour = stoi( currLineSplitted[3] );
-				global::expansion_scenario_id = stoi( currLineSplitted[4] );
-				global::exp_pv_kWp      = stof( currLineSplitted[5] );
-				global::exp_bess_kW     = stof( currLineSplitted[6] );
-				global::exp_bess_kWh    = stof( currLineSplitted[7] );
-				global::exp_bess_start_soc    = stof( currLineSplitted[8] );
-
-				global::ts_start_str_init    = true;
-				global::ts_end_str_init      = true;
-				global::tsteps_per_hour_init = true;
-				global::expansion_scenario_id_init = true;
-				global::exp_pv_kWp_init      = true;
-				global::exp_bess_kW_init     = true;
-				global::exp_bess_kWh_init    = true;
-				global::exp_bess_start_soc_init    = true;
-
+				Global::set_ts_start_str(   new string( currLineSplitted[1] )); // copy constructor
+				Global::set_ts_end_str(     new string( currLineSplitted[2] )); // copy constructor
+				Global::set_tsteps_per_hour(stoi( currLineSplitted[3] ));
+				Global::set_expansion_scenario_id(stoi( currLineSplitted[4] ));
+				Global::set_exp_pv_kWp(        stof( currLineSplitted[5] ));
+				Global::set_exp_bess_kW(       stof( currLineSplitted[6] ));
+				Global::set_exp_bess_kWh(      stof( currLineSplitted[7] ));
+				Global::set_exp_bess_start_soc(stof( currLineSplitted[8] ));
 				break;
 			} else {
 				continue;
@@ -73,36 +64,117 @@ bool configld::parse_scenario_file(int scenario_id) {
 
 
 int load_data_from_central_database_callbackA(void* data, int argc, char** argv, char** colName) {
-	/* argc: holds the number of results, argv: holds each value in array, colName: holds each column returned in array, */
+	/* 
+	 * This is the callback function for reading global attributes
+	 * (like timesteps and substations available in the data)
+	 * 
+	 * Parameter information taken from documentation:
+	 *   argc: holds the number of results
+	 *   argv: holds each value in array
+	 *   colName: holds each column returned in array
+	 */
 	string comparisonStr1 = "n_timesteps";
 	string comparisonStr2 = "n_substations";
+	string comparisonStr3 = "n_control_units";
+	string comparisonStr4 = "n_measurement_units";
 	if (argc != 2) {
-		cout << "Number of arguments not equal to 2 for one column!";
+		cerr << "Number of arguments not equal to 2 for one row!" << endl;
 		return 1;
 	}
 	
 	if        (comparisonStr1.compare(argv[0]) == 0) {
-		global::n_timesteps        = stoi(argv[1]);
-		global::n_timesteps_init   = true;
+		Global::set_n_timesteps(   stoi(argv[1]) );
 	} else if (comparisonStr2.compare(argv[0]) == 0) {
-		global::n_substations      = stoi(argv[1]);
-		global::n_substations_init = true;
+		Global::set_n_substations( stoi(argv[1]) );
+	} else if (comparisonStr3.compare(argv[0]) == 0) {
+		Global::set_n_CUs(          stoi(argv[1]) );
+	} else if (comparisonStr4.compare(argv[0]) == 0) {
+		Global::set_n_MUs(          stoi(argv[1]) );
 	}
 
 	return 0;
 }
 int load_data_from_central_database_callbackB(void* data, int argc, char** argv, char** colName) {
-	/* argc: holds the number of results, argv: holds each value in array, colName: holds each column returned in array, */
+	/* 
+	 * This is the callback function for loading the time indices
+	 */
     static int callcounter = 1;
     int pos = callcounter - 1; // current position in the array is one behind the count of calls
 	if (argc != 3) {
-		cout << "Number of arguments not equal to 3 for one column!";
+		cerr << "Number of arguments not equal to 3 for one row!" << endl;
 		return 1;
 	}
-    global::time_timestep_id[pos] = stoi(argv[0]);
+	int current_time_index = stoi(argv[0]);
+	if (current_time_index != callcounter) {
+		cerr << "Time indices are not ordered sequentially!" << endl;
+		return 1;
+	}
+    global::time_timestep_id[pos] = current_time_index;
     global::time_localtime_str->push_back(string(argv[1]));
     global::time_localtimezone_str->push_back(string(argv[2]));
     callcounter++;
+	return 0;
+}
+int load_data_from_central_database_callbackC(void* data, int argc, char** argv, char** colName) {
+	/* 
+	 * This is the callback function for geeting information about the substations
+	 * out of the database.
+	 * This function also creates the substations.
+	 */
+	if (argc != 2) {
+		cerr << "Number of arguments not equal to 2 for one row!" << endl;
+		return 1;
+	}
+	int current_station_id = stoi(argv[0]);
+	string* stationName = new string(argv[1]);
+	try {
+    	/*Substation* newSubstation = */ new Substation(current_station_id, stationName);
+	} catch (const char* msg) {
+		return 1;
+	}
+	return 0;
+}
+int load_data_from_central_database_callbackD(void* data, int argc, char** argv, char** colName) {
+	/* 
+	 * This is the callback function for geeting information about the control units
+	 * out of the database.
+	 * This function also creates the control units.
+	 */
+	if (argc != 2) {
+		cerr << "Number of arguments not equal to 2 for one row!" << endl;
+		return 1;
+	}
+	int current_cu_id = stoi(argv[0]);
+	int conn_to_subst_id = stoi(argv[1]);
+	try {
+    	/*ControlUnit* newCU = */ new ControlUnit(current_cu_id, conn_to_subst_id);
+	} catch (const char* msg) {
+		return 1;
+	}
+	return 0;
+}
+int load_data_from_central_database_callbackE(void* data, int argc, char** argv, char** colName) {
+	/* 
+	 * This is the callback function for geeting information about the measurement units
+	 * out of the database.
+	 * This function also creates the measurement units.
+	 * 
+	 * 0        1       2     3           4           5
+	 * MELO_ID, UnitID, MELO, has_demand, has_feedin, has_pv_residential
+	 * 6                  7         8       9
+	 * has_pv_open_space, has_bess, has_hp, has_chp
+	 */
+	if (argc != 10) {
+		cerr << "Number of arguments not equal to 10 for one row!" << endl;
+		return 1;
+	}
+	int current_mu_id = stoi(argv[0]);
+	//
+	// TODO TODO TODO !!!
+	//
+	/* TODO string* stationName = new string(argv[1]);
+    Substation* newSubstation = new Substation(stationName);
+	global::substation_list[pos] = newSubstation;*/
 	return 0;
 }
 bool configld::load_data_from_central_database(const char* filepath) {
@@ -119,14 +191,15 @@ bool configld::load_data_from_central_database(const char* filepath) {
 		char* sqlErrorMsgA;
 		int ret_valA = sqlite3_exec(dbcon, sql_queryA.c_str(), load_data_from_central_database_callbackA, NULL, &sqlErrorMsgA);
 		if (ret_valA != 0) {
-			cout << "Error when reading the SQL-Table: " << sqlErrorMsgA;
+			cerr << "Error when reading the SQL-Table: " << sqlErrorMsgA;
 			sqlite3_free(sqlErrorMsgA);
+			return false;
 		}
 
         //
         // Initialize global time list
         //
-        global::time_timestep_id = new int[global::n_timesteps];
+        global::time_timestep_id = new int[Global::get_n_timesteps()];
         global::time_localtime_str = new vector<string>();
         global::time_localtimezone_str = new vector<string>();
         //
@@ -136,19 +209,52 @@ bool configld::load_data_from_central_database(const char* filepath) {
 		char* sqlErrorMsgB;
 		int ret_valB = sqlite3_exec(dbcon, sql_queryB.c_str(), load_data_from_central_database_callbackB, NULL, &sqlErrorMsgB);
 		if (ret_valB != 0) {
-			cout << "Error when reading the SQL-Table: " << sqlErrorMsgB;
+			cerr << "Error when reading the SQL-Table: " << sqlErrorMsgB;
 			sqlite3_free(sqlErrorMsgB);
+			return false;
 		}
         //
         global::time_info_init = true;
-        // TODO: Check time indices, are they continuos?
 		
+		//
+        // Initialize global list of units (i.e. control unit, measurement unit and substation)
+        //
+		Substation::InitializeStaticVariables(Global::get_n_substations());
+		ControlUnit::InitializeStaticVariables(Global::get_n_CUs());
+		MeasurementUnit::InitializeStaticVariables(Global::get_n_MUs());
         //
         // Load component information and create units
         // 1. per substation
         // 2. per control unit
         // 3. per measurement unit
         //
+		// 1. substations
+		string sql_queryC = "SELECT substation_id, substation_name FROM substation_information ORDER BY substation_id;";
+		char* sqlErrorMsgC;
+		int ret_valC = sqlite3_exec(dbcon, sql_queryC.c_str(), load_data_from_central_database_callbackC, NULL, &sqlErrorMsgC);
+		if (ret_valC != 0) {
+			cerr << "Error when reading the SQL-Table: " << sqlErrorMsgC;
+			sqlite3_free(sqlErrorMsgC);
+			return false;
+		}
+		// 2. CUs
+		string sql_queryD = "SELECT UnitID, substation_id FROM melo_information ORDER BY UnitID;";
+		char* sqlErrorMsgD;
+		int ret_valD = sqlite3_exec(dbcon, sql_queryD.c_str(), load_data_from_central_database_callbackD, NULL, &sqlErrorMsgD);
+		if (ret_valD != 0) {
+			cerr << "Error when reading the SQL-Table: " << sqlErrorMsgD;
+			sqlite3_free(sqlErrorMsgD);
+			return false;
+		}
+		// 3. MUs
+		string sql_queryE = "SELECT MELO_ID, UnitID, MELO, has_demand, has_feedin, has_pv_residential, has_pv_open_space, has_bess, has_hp, has_chp FROM melo_information ORDER BY MELO_ID;";
+		char* sqlErrorMsgE;
+		int ret_valE = sqlite3_exec(dbcon, sql_queryE.c_str(), load_data_from_central_database_callbackE, NULL, &sqlErrorMsgE);
+		if (ret_valE != 0) {
+			cerr << "Error when reading the SQL-Table: " << sqlErrorMsgE;
+			sqlite3_free(sqlErrorMsgE);
+			return false;
+		}
         
 		//
         // Load address data
@@ -157,7 +263,7 @@ bool configld::load_data_from_central_database(const char* filepath) {
 		sqlite3_close(dbcon);
         return true;
 	} else {
-		cout << "Error when connecting to the database" << endl;
+		cerr << "Error when connecting to the database" << endl;
 		return false;
 	}
 }
@@ -233,7 +339,7 @@ bool expansion::load_expansion_matrix(float expansion_matrix[16][16]) {
 	ifstream expmat_input;
 	stringstream expmat_input_path;
 	expmat_input_path << "../config/expansion_scenarios/";
-	expmat_input_path << setw(4) << setfill('0') << global::expansion_scenario_id;
+	expmat_input_path << setw(4) << setfill('0') << Global::get_expansion_scenario_id();
 	expmat_input_path << ".csv";
 	expmat_input.open(expmat_input_path.str());
 	if (!expmat_input.good()) {
