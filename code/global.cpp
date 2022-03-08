1ce66f3923248bb16bc6816d3f44a18fcaf74ab8
Daniel Rene Bayer <daniel.bayer@uni-wuerzburg.de>
2022-03-08 16:28:03 +0100

    Global variables, that are constant after init, moved to own class

--- code/global.cpp
+++ code/global.cpp
@@ -1,10 +1,79 @@
 #include "global.h"
 
 using namespace global;
-    
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
 bool global::all_variables_initialized() {
+    if (time_info_init)
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void global::vacuum() {
+    delete[] time_timestep_id;       time_timestep_id       = NULL;
+    delete   time_localtime_str;     time_localtime_str     = NULL;
+    delete   time_localtimezone_str; time_localtimezone_str = NULL;
+}
+
+
+
+
+
+// ----------------------------- //
+//      Implementation of        //
+//            Global             //
+// ----------------------------- //
+
+int Global::n_timesteps           = 0;
+int Global::n_substations         = 0;
+int Global::n_CUs                 = 0;
+int Global::n_MUs                 = 0;
+string* Global::ts_start_str          = NULL;
+string* Global::ts_end_str            = NULL;
+int Global::tsteps_per_hour       = 1;
+int Global::expansion_scenario_id = 0;
+float Global::exp_pv_kWp            = 0.0;
+float Global::exp_bess_kW           = 0.0;
+float Global::exp_bess_kWh          = 0.0;
+float Global::exp_bess_start_soc    = 0.0;
+//
+bool Global::n_timesteps_init      = false;
+bool Global::n_substations_init    = false;
+bool Global::n_CUs_init            = false;
+bool Global::n_MUs_init            = false;
+bool Global::ts_start_str_init     = false;
+bool Global::ts_end_str_init       = false;
+bool Global::tsteps_per_hour_init  = false;
+bool Global::expansion_scenario_id_init = false;
+bool Global::exp_pv_kWp_init       = false;
+bool Global::exp_bess_kW_init      = false;
+bool Global::exp_bess_kWh_init     = false;
+bool Global::exp_bess_start_soc_init    = false;
+
+void Global::InitializeStaticVariables() {
+    // nothing to do anymore
+}
+
+void Global::DeleteStaticVariables() {
+    delete ts_start_str;
+    delete ts_end_str;
+}
+
+bool Global::AllVariablesInitialized() {
     if (n_timesteps_init && 
 	    n_substations_init &&
+        n_CUs_init &&
+        n_MUs_init &&
 		ts_start_str_init &&
 	    ts_end_str_init &&
 	    tsteps_per_hour_init &&
@@ -21,12 +90,138 @@ bool global::all_variables_initialized() {
         
 }
 
-void global::vacuum() {
-    delete   ts_start_str;           ts_start_str           = NULL;
-    delete   ts_end_str;             ts_end_str             = NULL;
-    delete[] time_timestep_id;       time_timestep_id       = NULL;
-    delete   time_localtime_str;     time_localtime_str     = NULL;
-    delete   time_localtimezone_str; time_localtimezone_str = NULL;
+/*
+inline int Global::get_n_timesteps() {
+	return n_timesteps;
 }
+inline int Global::get_n_substations() {
+	return n_substations;
+}
+inline int Global::get_n_CUs() {
+	return n_CUs;
+}
+inline int Global::get_n_MUs() {
+	return n_MUs;
+}
+inline std::string* Global::get_ts_start_str() {
+	return ts_start_str;
+}
+inline std::string* Global::get_ts_end_str() {
+	return ts_end_str;
+}
+inline int Global::get_tsteps_per_hour() {
+	return tsteps_per_hour;
+}
+inline int Global::get_expansion_scenario_id() {
+	return expansion_scenario_id;
+}
+inline float Global::get_exp_pv_kWp() {
+	return exp_pv_kWp;
+}
+inline float Global::get_exp_bess_kW() {
+	return exp_bess_kW;
+}
+inline float Global::get_exp_bess_kWh() {
+	return exp_bess_kWh;
+}
+inline float Global::get_exp_bess_start_soc() {
+	return exp_bess_start_soc;
+}
+*/
 
-
+void Global::set_n_timesteps(int n_timesteps) {
+    if (n_timesteps_init) {
+        cerr << "Global variable n_timesteps is already initialized!" << endl;
+    } else {
+        Global::n_timesteps = n_timesteps;
+        Global::n_timesteps_init = true;
+    }
+}
+void Global::set_n_substations(int n_substations) {
+    if (n_substations_init) {
+        cerr << "Global variable n_substations is already initialized!" << endl;
+    } else {
+        Global::n_substations = n_substations;
+        Global::n_substations_init = true;
+    }
+}
+void Global::set_n_CUs(int n_CUs) {
+    if (n_CUs_init) {
+        cerr << "Global variable n_CUs is already initialized!" << endl;
+    } else {
+        Global::n_CUs = n_CUs;
+        Global::n_CUs_init = true;
+    }
+}
+void Global::set_n_MUs(int n_MUs) {
+    if (n_MUs_init) {
+        cerr << "Global variable n_MUs is already initialized!" << endl;
+    } else {
+        Global::n_MUs = n_MUs;
+        Global::n_MUs_init = true;
+    }
+}
+void Global::set_ts_start_str(std::string* ts_start_str) {
+    if (ts_start_str_init) {
+        cerr << "Global variable ts_start_str is already initialized!" << endl;
+    } else {
+        Global::ts_start_str = ts_start_str;
+        Global::ts_start_str_init = true;
+    }
+}
+void Global::set_ts_end_str(std::string* ts_end_str) {
+    if (ts_end_str_init) {
+        cerr << "Global variable ts_end_str is already initialized!" << endl;
+    } else {
+        Global::ts_end_str = ts_end_str;
+        Global::ts_end_str_init = true;
+    }
+}
+void Global::set_tsteps_per_hour(int tsteps_per_hour) {
+    if (tsteps_per_hour_init) {
+        cerr << "Global variable tsteps_per_hour is already initialized!" << endl;
+    } else {
+        Global::tsteps_per_hour = tsteps_per_hour;
+        Global::tsteps_per_hour_init = true;
+    }
+}
+void Global::set_expansion_scenario_id(int expansion_scenario_id) {
+    if (expansion_scenario_id_init) {
+        cerr << "Global variable expansion_scenario_id is already initialized!" << endl;
+    } else {
+        Global::expansion_scenario_id = expansion_scenario_id;
+        Global::expansion_scenario_id_init = true;
+    }
+}
+void Global::set_exp_pv_kWp(float exp_pv_kWp) {
+    if (exp_pv_kWp_init) {
+        cerr << "Global variable exp_pv_kWp is already initialized!" << endl;
+    } else {
+        Global::exp_pv_kWp = exp_pv_kWp;
+        Global::exp_pv_kWp_init = true;
+    }
+}
+void Global::set_exp_bess_kW(float exp_bess_kW) {
+    if (exp_bess_kW_init) {
+        cerr << "Global variable exp_bess_kW is already initialized!" << endl;
+    } else {
+        Global::exp_bess_kW = exp_bess_kW;
+        Global::exp_bess_kW_init = true;
+    }
+}
+void Global::set_exp_bess_kWh(float exp_bess_kWh) {
+    if (exp_bess_kWh_init) {
+        cerr << "Global variable exp_bess_kWh is already initialized!" << endl;
+    } else {
+        Global::exp_bess_kWh = exp_bess_kWh;
+        Global::exp_bess_kWh_init = true;
+    }
+}
+void Global::set_exp_bess_start_soc(float exp_bess_start_soc) {
+    if (exp_bess_start_soc_init) {
+        cerr << "Global variable exp_bess_start_soc is already initialized!" << endl;
+    } else {
+        Global::exp_bess_start_soc = exp_bess_start_soc;
+        Global::exp_bess_start_soc_init = true;
+    }
+}
--- code/global.h
+++ code/global.h
@@ -12,33 +12,28 @@
 #include <string>
 #include <vector>
 
-namespace global {
+#include "units.h"
 
-    inline int n_timesteps   = 0; ///< Total number of timesteps for which data is available
-    inline int n_substations = 0; ///< Total number of substations for which data is available
-    inline std::string* ts_start_str = NULL; ///< String of the start date
-    inline std::string* ts_end_str   = NULL; ///< String of the end date
-    inline int tsteps_per_hour      = 1;    ///< Time steps per hour in the simulation (and the data!)
-    inline int expansion_scenario_id= 0;    ///< ID of the expansion scenario
-    inline float exp_pv_kWp         = 0.0;  ///< kWp of in the simulation added PV installations
-    inline float exp_bess_kW        = 0.0;  ///< P [kW] of in the simulation added BESS installations
-    inline float exp_bess_kWh       = 0.0;  ///< E [kWh] of in the simulation added BESS installations
-    inline float exp_bess_start_soc = 0.0;  ///< SOC at the beginning of the simulation for newly added BESS installations
+namespace global {
+    /*
+     * Namespace global
+     *
+     * It contains all global attributes and variables that
+     * might change during simulation execution.
+     * 
+     * Attention: There is no access protection for these variables!
+     * For access protection use class Global.
+     * 
+     * Attention: Do not confuse with class Global (mind the capital "G")!
+     */
+
+    // inline float current_energy_feedin_price = 0.0;
+    // inline float current_energy_supply_price = 0.0;
     // Time info (read from time_indices table in central values database)
     inline int* time_timestep_id = NULL; ///< Reference to the list of time steps
     inline std::vector<std::string>* time_localtime_str     = NULL; ///< Reference to the list of the time as string - alignment fits to time_timestep_id
     inline std::vector<std::string>* time_localtimezone_str = NULL; ///< Reference to the list of the time zone as string - alignment fits to time_timestep_id
 
-    inline bool n_timesteps_init     = false;
-    inline bool n_substations_init   = false;
-    inline bool ts_start_str_init    = false;
-    inline bool ts_end_str_init      = false;
-    inline bool tsteps_per_hour_init = false;
-    inline bool expansion_scenario_id_init = false;
-    inline bool exp_pv_kWp_init      = false;
-    inline bool exp_bess_kW_init     = false;
-    inline bool exp_bess_kWh_init    = false;
-    inline bool exp_bess_start_soc_init    = false;
     inline bool time_info_init       = false;
 
 
@@ -46,6 +41,92 @@ namespace global {
     void vacuum();  ///< Deletes all global variables in the end
 
 }
+
+class Global {
+    /*
+     * class Global
+     *
+     * This class contains all global variables that cannot change
+     * after they have been set once.
+     * 
+     * Attention: Not to be confused with namespace global (mind the lower case "g").
+     */
+    public:
+        static void InitializeStaticVariables();
+        static void DeleteStaticVariables();
+        //
+        static bool AllVariablesInitialized();
+        //
+        // getter methods
+        /*
+        static inline int get_n_timesteps();
+        static inline int get_n_substations();
+        static inline int get_n_CUs();
+        static inline int get_n_MUs();
+        static inline std::string* get_ts_start_str();
+        static inline std::string* get_ts_end_str();
+        static inline int get_tsteps_per_hour();
+        static inline int get_expansion_scenario_id();
+        static inline float get_exp_pv_kWp();
+        static inline float get_exp_bess_kW();
+        static inline float get_exp_bess_kWh();
+        static inline float get_exp_bess_start_soc();
+        */
+        static int get_n_timesteps()   {  return n_timesteps; }
+        static int get_n_substations() {  return n_substations; }
+        static int get_n_CUs()         {  return n_CUs; }
+        static int get_n_MUs()         {  return n_MUs; }
+        static std::string* get_ts_start_str() { return ts_start_str;    }
+        static std::string* get_ts_end_str()   { return ts_end_str;    }
+        static int get_tsteps_per_hour()       { return tsteps_per_hour;    }
+        static int get_expansion_scenario_id() { return expansion_scenario_id;    }
+        static float get_exp_pv_kWp()          { return exp_pv_kWp;    }
+        static float get_exp_bess_kW()         { return exp_bess_kW;    }
+        static float get_exp_bess_kWh()        { return exp_bess_kWh;    }
+        static float get_exp_bess_start_soc()  { return exp_bess_start_soc;    }
+        // setter methods
+        static void set_n_timesteps(int n_timesteps);
+        static void set_n_substations(int n_substations);
+        static void set_n_CUs(int n_CUs);
+        static void set_n_MUs(int n_MUs);
+        static void set_ts_start_str(std::string* ts_start_str);
+        static void set_ts_end_str(std::string* ts_end_str);
+        static void set_tsteps_per_hour(int tsteps_per_hour);
+        static void set_expansion_scenario_id(int expansion_scenario_id);
+        static void set_exp_pv_kWp(float exp_pv_kWp);
+        static void set_exp_bess_kW(float exp_bess_kW);
+        static void set_exp_bess_kWh(float exp_bess_kWh);
+        static void set_exp_bess_start_soc(float exp_bess_start_soc);
+    private:
+        Global(); ///< Global cannot be initialized, it is a static only class
+        // variables
+        static int n_timesteps;            ///< Total number of timesteps for which data is available
+        static int n_substations;          ///< Total number of substations for which data is available
+        static int n_CUs;                  ///< Total number of control units for which data is available
+        static int n_MUs;                  ///< Total number of meausrement units for which data is available
+        static std::string* ts_start_str;  ///< String of the start date
+        static std::string* ts_end_str;    ///< String of the end date
+        static int tsteps_per_hour;        ///< Time steps per hour in the simulation (and the data!)
+        static int expansion_scenario_id;  ///< ID of the expansion scenario
+        static float exp_pv_kWp;           ///< kWp of in the simulation added PV installations
+        static float exp_bess_kW;          ///< P [kW] of in the simulation added BESS installations
+        static float exp_bess_kWh;         ///< E [kWh] of in the simulation added BESS installations
+        static float exp_bess_start_soc;   ///< SOC at the beginning of the simulation for newly added BESS installations
+        // boolean values holding information if the correspoding 
+        // variable has been set or not
+        static bool n_timesteps_init;
+        static bool n_substations_init;
+        static bool n_CUs_init;
+        static bool n_MUs_init;
+        static bool ts_start_str_init;
+        static bool ts_end_str_init;
+        static bool tsteps_per_hour_init;
+        static bool expansion_scenario_id_init;
+        static bool exp_pv_kWp_init;
+        static bool exp_bess_kW_init;
+        static bool exp_bess_kWh_init;
+        static bool exp_bess_start_soc_init;
+};
 
 #endif
 
