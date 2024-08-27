65cf08d1eda45b341b5252308e1df349dd683154
Daniel Rene Bayer <daniel.bayer@uni-wuerzburg.de>
2024-08-27 16:13:14 +0200

    Compiled program files moved to new folder 'bin', so that it is not outputted to 'code' anymore

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -20,32 +20,32 @@ output_error=0   # Outputs do not match the verified output
 
 # 1. run simulations
 echo -e "\n-- 1. Simulation run --"
-../code/main-opti --config ../test/test-config/test_config.json 1
+../bin/simulation-opti --config ../test/test-config/test_config.json 1
 if (( $? != 0 )); then program_error=1; fi
 echo -e "\n-- 2. Simulation run --"
-../code/main-opti --config ../test/test-config/test_config.json 2
+../bin/simulation-opti --config ../test/test-config/test_config.json 2
 if (( $? != 0 )); then program_error=1; fi
 echo -e "\n-- 3. Simulation run --"
-../code/main-opti --config ../test/test-config/test_config.json 3
+../bin/simulation-opti --config ../test/test-config/test_config.json 3
 if (( $? != 0 )); then program_error=1; fi
 echo -e "\n-- 4. Simulation run --"
-../code/main-opti --config ../test/test-config/test_config.json 4
+../bin/simulation-opti --config ../test/test-config/test_config.json 4
 if (( $? != 0 )); then program_error=1; fi
 echo -e "\n-- 5. Simulation run --"
-../code/main-opti --config ../test/test-config/test_config.json 5
+../bin/simulation-opti --config ../test/test-config/test_config.json 5
 if (( $? != 0 )); then program_error=1; fi
 echo -e "\n-- 6. Simulation run --"
-../code/main-opti --config ../test/test-config/test_config.json --pvar 1 7
+../bin/simulation-opti --config ../test/test-config/test_config.json --pvar 1 7
 if (( $? != 0 )); then program_error=1; fi
 echo -e "\n-- 7. Simulation run --"
-../code/main-opti --config ../test/test-config/test_config.json --seed 1234 8
+../bin/simulation-opti --config ../test/test-config/test_config.json --seed 1234 8
 if (( $? != 0 )); then program_error=1; fi
 
 
 # use valgrind for last run, but only, if there are no errors before
 if (( $program_error <= 0 )); then
     echo -e "\n-- 8. Simulation run (using valgrind) --"
-    valgrind --error-exitcode=1 ../code/main-dbg --config ../test/test-config/test_config.json --seed 1234 6
+    valgrind --error-exitcode=1 ../bin/simulation-dbg --config ../test/test-config/test_config.json --seed 1234 6
     if (( $? != 0 )); then memory_error=1; else memory_error=0;  fi
 fi
 
